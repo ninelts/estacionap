8f45db579a7c82b674e73ffc7b0977590e686f53
fjoost-ubuntu <fjoostr@gmail.com>
2019-07-18 09:21:19 -0400

    probando crud tariff

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -81,23 +81,24 @@
                         </li>
                         <li>
                             <a href="#">
-                                <i class="fas fa-chart-line"></i>
-                                Ventas
-                            </a>
-                            <ul>
-                                <li>
-                                    <a href="#">
-                                        <i class="fas fa-list"></i>
-                                        Ver Todos
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fas fa-plus"></i>
-                                        Agregar Nuevo
-                                    </a>
-                                </li>
-                            </ul>
+                                <a href="{{route('listado_usuarios')}}">
+                                    <i class="fas fa-list"></i>
+                                    Ver Todos
+                                </a>
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fas fa-list"></i>
+                                            Ver Todos
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fas fa-plus"></i>
+                                            Agregar Nuevo
+                                        </a>
+                                    </li>
+                                </ul>
                         </li>
                         <li>
                             <a href="#">
